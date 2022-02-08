96e9c99f9183ff639d95ac90a2266d04913a9f8c
Halirahmanov <insaf.halirahmanov@ispring.com>
2022-02-08 16:17:56 +0300

    Front-5. Обработка позиции мыши

--- client/src/components/Canvas/Canvas.tsx
+++ client/src/components/Canvas/Canvas.tsx
@@ -1,20 +1,44 @@
 import classes from './Canvas.module.css'
 import {useAtom} from "@reatom/react";
 import {selectedToolAtom} from "../../model/tools";
-import {useEffect, useRef} from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
 
 function Canvas() {
     const [tool] = useAtom(selectedToolAtom)
     const canvasRef = useRef<HTMLCanvasElement>(null)
     const mousePressed = useRef(false)
+    const startPosition = useRef({
+        x: 0,
+        y: 0
+    })
+    const endPosition = useRef({
+        x: 0,
+        y: 0
+    })
+
+    const getMousePosition = useCallback((e: MouseEvent) => {
+        let top = 0, left = 0
+        if (canvasRef.current) {
+            const bounding = canvasRef.current.getBoundingClientRect()
+            top = bounding.top
+            left = bounding.left
+        }
+        return {
+            x: e.clientX - left,
+            y: e.clientY - top
+        }
+    }, [canvasRef])
 
     useEffect(() => {
         if (!canvasRef.current) return
 
         const listeners = {
             onMouseDownHandler: (e: MouseEvent) => {
+                if (!canvasRef.current) return
+                const {top, left} = canvasRef.current.getBoundingClientRect()
                 mousePressed.current = true
-                console.log(e, tool)
+                startPosition.current = getMousePosition(e)
+                console.log(startPosition.current)
             },
             onMouseMoveHandler: (e: MouseEvent) => {
                 if (mousePressed.current) {
@@ -24,6 +48,8 @@ function Canvas() {
             onMouseUpHandler: (e: MouseEvent) => {
                 console.log(e, tool)
                 mousePressed.current = false
+                endPosition.current = getMousePosition(e)
+                console.log(endPosition.current)
             }
         }
 
