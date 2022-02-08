ca0373e99c1926b99471e7d18342318b31766aab
Halirahmanov <insaf.halirahmanov@ispring.com>
2022-02-08 14:22:17 +0300

    Front-2. Добавил reatom в проект

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import {Toolbar} from "./components/Toolbar/Toolbar"
 import {Canvas} from "./components/Canvas/Canvas"
-import classes from "./App.module.css";
+import classes from "./App.module.css"
+import {createStore} from "@reatom/core"
+import {reatomContext} from '@reatom/react'
 
 function App() {
+    const store = createStore()
+
     return (
     <div className={classes.app}>
-        <Toolbar />
-        <Canvas />
+        <reatomContext.Provider value={store}>
+            <Toolbar />
+            <Canvas />
+        </reatomContext.Provider>
     </div>
   )
 }
